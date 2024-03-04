a8f2d1388141997959ad07646f84d493f9e69709
Alba María Romero <albamrm@gmail.com>
2024-03-04 09:57:34 +0100

    emojis

---
+++ import_emoji_alba.py
@@ -0,0 +1,27 @@
+"""
+# UNICODE
+
+print("\U0001F7E6") # Color azul
+print("\U00002B1B") # Color negro
+print("\U0001F7E5") # Color rojo
+print("\U00002B1C") # Color blanco
+"""
+
+"""
+# NOMBRES CLDR
+
+print("\N{blue square}") # Color azul (No consigo que funcione)
+print("\N{black large square}") # Color negro
+print("\N{red square}") # Color rojo (No consigo que funcione)
+print("\N{white large square}") # Color blanco
+"""
+
+"""
+# MODULO EMOJI
+
+import emoji
+print(emoji.emojize(":blue_square:")) # Color azul
+print(emoji.emojize(":black_large_square:")) # Color negro
+print(emoji.emojize(":red_square:")) # Color rojo
+print(emoji.emojize(":white_large_square:")) # Color blanco
+"""
