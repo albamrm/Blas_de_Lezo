c2f330eb2726716cfd4985f0f5948a97cb193c01
PIPM2001 <piperezmirads@gmail.com>
2024-03-04 17:32:17 +0100

    Creo ficheros python

---
+++ Fincheros Python/main.py
@@ -0,0 +1,27 @@
+import numpy as np
+import sys
+
+from variables import*
+from clases import*
+from funciones import*
+
+"""
+CÓDIGO PRINCIPAL
+def main():
+    #args = sys.argv[1:]
+    #for arg in args:
+    #    print(f"Has llamado al main con argumento {arg}")
+
+    print("Hello World!")
+    
+    print(f"La dimensión del tablero es {DIMENSION_TABLERO}")
+    
+    funcion1()
+    print(funcion2(10,10))
+    
+    objeto = ClaseA()
+
+
+if __name__ == '__main__':
+    main()
+"""
